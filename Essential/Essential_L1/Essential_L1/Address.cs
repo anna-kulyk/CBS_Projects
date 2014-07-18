7920346fae9eaa6e814485007e09b638d1253e91
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-18 19:04:02 -0400

    Implemented Address properties Fixed project

--- Essential/Essential_L1/Essential_L1/Address.cs
+++ Essential/Essential_L1/Essential_L1/Address.cs
@@ -8,7 +8,11 @@ namespace Essential_L1
 {
     class Address
     {
-
-
+        public uint Index { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Appartment { get; set; }
     }
 }
