82a8ee1379abb3f01b8284472f37d07c788b5aad
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-20 13:45:51 -0400

    Add project Essential_L1.1. Implement Rectangle.cs.

---
+++ Essential/Essential_L1/Essential_L1.1/Rectangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L1._1
+{
+    class Rectangle
+    {
+        double _side1, _side2;
+
+        public Rectangle(double side1, double side2)
+        {
+            _side1 = side1;
+            _side2 = side2;
+        }
+
+        public double Area
+        {
+            get
+            {
+                return AreaCalculator();
+            }
+        }
+
+        public double Perimeter
+        {
+            get
+            {
+                return PerimeterCalculator();
+            }
+        }
+
+        private double AreaCalculator()
+        {
+            return (_side1 * _side2);
+        }
+
+        private double PerimeterCalculator()
+        {
+            return (_side1 + _side2) * 2;
+        }
+
+    }
+}
