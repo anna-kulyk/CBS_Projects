fa9e93376e1282683f6894276f88b54763c80a5e
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-20 13:52:37 -0400

    Add Consol output of Area and Perimeter. Update .gitignore.

--- Essential/Essential_L1/Essential_L1.1/Program.cs
+++ Essential/Essential_L1/Essential_L1.1/Program.cs
@@ -10,6 +10,9 @@ namespace Essential_L1._1
     {
         static void Main(string[] args)
         {
+            var myRectangle = new Rectangle(2, 4);
+            Console.WriteLine("Площадь прямоугольнока равна: {0}", myRectangle.Area);
+            Console.WriteLine("Периметр прямоугольнока равен: {0}", myRectangle.Perimeter);
         }
     }
 }
