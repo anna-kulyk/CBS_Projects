a1e43e2eccb4186a6c593ee9096535febfbc3335
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-21 13:52:54 -0400

    Implement project Essential_L2.1 Add and implement class Converter.cs Update gitignore

---
+++ Essential/Essential_L2/Essential_L2.1/Converter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L2._1
+{
+    class Converter
+    {
+        public Converter() { }
+
+        public Converter (double usd, double eur, double rub)
+        {
+            Usd = usd;
+            Eur = eur;
+            Rub = rub;
+        }
+
+        public double Usd { get; private set; }
+        public double Eur { get; private set; }
+        public double Rub { get; private set; }
+
+        public void CurrencyConverter(string currencyFrom, string currencyTo, double sum)
+        {
+            double result;
+
+            if (currencyFrom.ToLower() == "uah")
+            {
+                switch (currencyTo.ToLower())
+                {
+                    case "usd":
+                        Console.WriteLine("{0:N} UAH equals to {1:N} {2}", sum, result = (sum / Usd),currencyTo.ToUpper());
+                        break;
+                    case "eur":
+                        Console.WriteLine("{0:N} UAH equals to {1:N} {2}", sum, result = (sum / Eur), currencyTo.ToUpper());
+                        break;
+                    case "rub":
+                        Console.WriteLine("{0:N} UAH equals to {1:N} {2}", sum, result = (sum / Rub), currencyTo.ToUpper());
+                        break;
+                    case "uah":
+                        Console.WriteLine("{0:N} UAH equals to {0:N} UAH", sum);
+                        break;
+                    default:
+                        Console.WriteLine("Error! The program cannot convert UAH to {0}!", currencyTo.ToUpper());
+                        break;
+                }
+
+            }
+            else if (currencyTo.ToLower() == "uah")
+            {
+                switch (currencyFrom.ToLower())
+                {
+                    case "usd":
+                        Console.WriteLine("{0:N} {2} equals to {1:N} UAH", sum, result = (sum * Usd), currencyFrom.ToUpper());
+                        break;
+                    case "eur":
+                        Console.WriteLine("{0:N} {2} equals to {1:N} UAH", sum, result = (sum * Eur), currencyFrom.ToUpper());
+                        break;
+                    case "rub":
+                        Console.WriteLine("{0:N} {2} equals to {1:N} UAH", sum, result = (sum * Rub), currencyFrom.ToUpper());
+                        break;
+                    case "uah":
+                        Console.WriteLine("{0:N} UAH equals to {0:N} UAH", sum);
+                        break;
+                    default:
+                        Console.WriteLine("Error! The program cannot convert {0} to UAH!", currencyFrom.ToUpper());
+                        break;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Error! One of the currencies should be hryvnia!");
+            }
+        }
+    }
+}
---
+++ Essential/Essential_L2/Essential_L2.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myMoney = new Converter(11.6556, 15.7642, 0.3315);
+
+            myMoney.CurrencyConverter("usd", "uah", 100);
+            Console.WriteLine(new String('-', 50));
+            myMoney.CurrencyConverter("uah", "uah", 100);
+            Console.WriteLine(new String('-', 50));
+            myMoney.CurrencyConverter("uah", "usd", 100);
+            Console.WriteLine(new String('-', 50));
+            myMoney.CurrencyConverter("uah", "rub", 100);
+            Console.WriteLine(new String('-', 50));
+            myMoney.CurrencyConverter("eur", "uah", 100);
+            Console.WriteLine(new String('-', 50));
+            myMoney.CurrencyConverter("uah", "evr", 100);
+        }
+    }
+}
