b4ec9f9d2b36db66dc7ec9f3e6fff76ff8ef56dc
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-22 15:32:17 -0400

    Implement project Essential_L3 Add and implement classes Printer.cs, Canon.cs, Epson.cs, HewlettPackard.cs Update gitignore

---
+++ Essential/Essential_L3/Essential_L3/HewlettPackard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L3
+{
+    class HewlettPackard : Printer
+    {
+        protected override ConsoleColor PrinterColor
+        {
+            get { return ConsoleColor.Red; }
+        }        
+    }
+}
---
+++ Essential/Essential_L3/Essential_L3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "Hello World!";
+
+            Printer[] printers = new Printer[]
+            {
+                new Canon(),
+                new Epson(),
+                new HewlettPackard()
+            };
+
+            foreach (var printer in printers)
+            {
+                printer.Print(test);
+            }
+        }
+    }
+}
