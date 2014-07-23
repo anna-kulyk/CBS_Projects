1a6097a69d5d9970ad4a3c517cfa516bc0c616d2
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-23 13:00:10 -0400

    Implement project Essential_L4 Add and implement classes Document.cs, DocumentParts.cs, Title.cs, Body.cs, Footer.cs Update gitignore

---
+++ Essential/Essential_L4/Essential_L4/Parts/Footer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L4
+{
+    class Footer : DocumentParts
+    {
+        public override string Content
+        {
+            get
+            {
+                if (content != null)
+                    return content;
+                else
+                    return "Нижний колонтитул отсутствует.";
+            }
+            set
+            {
+                content = value;
+            }
+        }
+
+        protected override ConsoleColor ShowForegroundColor
+        {
+            get
+            {
+                return ConsoleColor.Green;
+            }
+        }
+    }
+}
---
+++ Essential/Essential_L4/Essential_L4/Parts/Title.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L4
+{
+    class Title : DocumentParts
+    {
+        public override string Content
+        {
+            get 
+            {
+                if (content != null)
+                    return content;
+                else
+                    return "Заголовок отсутствует.";
+            }
+            set 
+            {
+                content = value; 
+            }
+        }
+
+        protected override ConsoleColor ShowForegroundColor
+        {
+            get
+            {
+                return ConsoleColor.Yellow;
+            }
+        }
+    }
+}
