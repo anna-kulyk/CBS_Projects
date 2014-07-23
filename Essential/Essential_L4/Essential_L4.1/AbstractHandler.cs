dfa7431abcb066f31aa277e885b755e1cb5ee125
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-23 15:42:00 -0400

    Implement project Essential_L4.1 Add and implement classes AbstractHandler.cs, TXTHandler.cs, DOCHandler.cs, XMLHandler.cs and enumerator DocumentType.cs Add method Handler in Program.cs Update gitignore

---
+++ Essential/Essential_L4/Essential_L4.1/AbstractHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L4._1
+{
+    abstract class AbstractHandler
+    {
+        abstract public void Open();
+        abstract public void Create();
+        abstract public void Change();
+        abstract public void Save();
+
+        public void Act()
+        {
+            Open();
+            Create();
+            Change();
+            Save();
+        }
+    }
+}
---
+++ Essential/Essential_L4/Essential_L4.1/DocumentType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L4._1
+{
+    enum DocumentType
+    {
+        None,
+        XML,
+        TXT,
+        DOC
+    }
+}
