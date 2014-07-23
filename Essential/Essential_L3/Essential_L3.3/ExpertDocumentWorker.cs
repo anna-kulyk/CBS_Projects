76fb8a73b7f63f4bb332576150f0780cc60a7757
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-23 10:31:25 -0400

    Implement project Essential_L3.3 Add and implement classes DocumentWorker.cs, ProDocumentWorker.cs and ExpertDocumentWorker.cs Update gitignore

---
+++ Essential/Essential_L3/Essential_L3.3/ExpertDocumentWorker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L3._3
+{
+    class ExpertDocumentWorker : ProDocumentWorker
+    {
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в новом формате");
+        }
+    }
+}
