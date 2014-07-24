d15f4ef28be745caf9248f52cd03584a84642695
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-24 13:07:54 -0400

    Implement project Essential_L5 Add and implement class Dictionary.cs and enumeration Language.cs Update gitignore

---
+++ Essential/Essential_L5/Essential_L5/Language.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L5
+{
+    enum Language
+    {
+        EN,
+        RU,
+        UA
+    }
+}
