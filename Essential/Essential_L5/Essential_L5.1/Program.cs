37b64883c8799fc1c4daadc080559fc6a4932dd1
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-24 15:39:18 -0400

    Implement project Essential_L5.1 Add and implement class MyArray.cs Update gitignore

---
+++ Essential/Essential_L5/Essential_L5.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myArrey = new MyArray();
+
+            Console.WriteLine("Max: {0}", myArrey.Max());
+            Console.WriteLine("Min: {0}", myArrey.Min());
+            Console.WriteLine("Sum: {0}", myArrey.Sum());
+            Console.WriteLine("Average: {0}", myArrey.Average());
+            var odds = myArrey.GetOdds();
+            Console.Write("Odds: ");
+            foreach (var odd in odds)
+            {
+                Console.Write(odd + "  ");
+            }
+        }
+    }
+}
