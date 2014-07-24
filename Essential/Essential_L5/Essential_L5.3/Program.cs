d3ad0b3b60b4a2c66c024f33016b03075baab8ae
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-24 19:36:42 -0400

    Implement project Essential_L5.3 Add and implement class Article.cs, Store.cs Update gitignore

---
+++ Essential/Essential_L5/Essential_L5.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myStore = new Store();
+            Console.WriteLine(myStore[1]);
+            Console.WriteLine(new string('-', 30));
+            Console.WriteLine(myStore[4]);
+            Console.WriteLine(new string('-', 30));
+            Console.Write("Enter the name of the article: ");
+            string s = Console.ReadLine();
+            Console.WriteLine(myStore[s]); ;
+        }
+    }
+}
