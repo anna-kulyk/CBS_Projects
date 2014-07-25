5b5b564dbbdc4ce0b420cf1e57efd7c89b6b3567
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-25 17:16:22 -0400

    Implement project Essential_L6.3 Add and implement static class ArraySorter.cs Update gitignore

---
+++ Essential/Essential_L6/Essential_L6.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L6._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 6, 4, 8, 8, 5, 1 };
+            array.LowToHigh();
+            foreach (var item in array)
+            {
+                Console.Write(item + "  ");                
+            }
+        }
+    }
+}
