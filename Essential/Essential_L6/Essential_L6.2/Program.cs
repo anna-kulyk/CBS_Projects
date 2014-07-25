426b98804207255d003e575643f048496f61e9cb
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-25 17:00:03 -0400

    Implement project Essential_L6.2 Add and implement nested class Notes Update gitignore

---
+++ Essential/Essential_L6/Essential_L6.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book.Notes.SaveNotes("Great book!");
+        }
+    }
+}
