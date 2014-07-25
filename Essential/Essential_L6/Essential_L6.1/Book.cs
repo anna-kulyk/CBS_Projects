544cf654bc95436d849ba216838fdc123fbeb1cc
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-25 16:43:43 -0400

    Implement project Essential_L6.1 Add and implement class FindAndReplaceManager.cs Update gitignore

---
+++ Essential/Essential_L6/Essential_L6.1/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L6._1
+{
+    class Book
+    {
+        public void FindNext(string str)
+        {
+            Console.WriteLine("Поиск строки : " + str);
+        }
+    }
+}
