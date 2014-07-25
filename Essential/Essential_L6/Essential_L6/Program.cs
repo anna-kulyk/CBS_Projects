af0c501279212067351f94e6078ad8567ad0bb77
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-25 16:18:47 -0400

    Implement project Essential_L6 Add and implement class Calculator.cs Update gitignore

---
+++ Essential/Essential_L6/Essential_L6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculator.Add(9, 3));
+            Console.WriteLine(Calculator.Subtract(9, 3));
+            Console.WriteLine(Calculator.Multiply(9, 3));
+            Console.WriteLine(Calculator.Divide(9, 3));
+        }
+    }
+}
