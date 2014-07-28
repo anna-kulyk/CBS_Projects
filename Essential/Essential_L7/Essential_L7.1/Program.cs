091373f1541ed946911f568c11dd456542a15033
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-28 16:07:12 -0400

    Implement project Essential_L7.1 Add and implement structure Train.cs Update gitignore

---
+++ Essential/Essential_L7/Essential_L7.1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L7._1
+{
+    class Program
+    {
+        static void AddTrains(Train[] trains)
+        {
+            for (int i = 0; i < trains.Length; i++)
+            {
+                Console.Write("Enter the destination: ");
+                string destination = Console.ReadLine();
+                Console.Write("Enter the number of the train: ");
+                uint number = Convert.ToUInt32(Console.ReadLine());
+                Console.Write("Enter the departure time: ");
+                string departureTime = Console.ReadLine();
+                Console.WriteLine(new string('-', 30));
+                trains[i] = new Train(destination, number, departureTime);
+            }
+        }
+
+        static void Sort(Train[] trains)
+        {            
+            for (int i = 0; i < trains.Length; i++)
+            {
+                for (int j = 0; j < trains.Length; j++)
+                {
+                    if (trains[i].Number <= trains[j].Number)
+                    {
+                        Train t = trains[i];
+                        trains[i] = trains[j];
+                        trains[j] = t;
+                        
+                    }
+                }
+            }
+        }
+
+        static void ShowTrainByNumber (Train[] trains, uint number)
+        {
+            int counter = 0;
+            for (int i = 0; i < trains.Length; i++)
+            {
+                if (trains[i].Number == number)
+                {
+                    counter++;
+                    Console.WriteLine(trains[i].ToString());
+                }                
+            }
+            if (counter == 0)
+            {
+               Console.WriteLine("The train # {0} doesn't exists!", number); 
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            var trains = new Train[3];
+            AddTrains(trains);
+            Sort(trains);
+            Console.WriteLine("You have entered the following data:");
+            foreach (var item in trains)
+            {
+                Console.WriteLine(new string('*', 20));
+                Console.WriteLine(item);                
+            }
+            Console.WriteLine(new string('-', 30));
+            Console.Write("Enter the number of the train to start the search: ");
+            uint tnumber = Convert.ToUInt32(Console.ReadLine());
+            ShowTrainByNumber(trains, tnumber);
+        }
+    }
+}
---
+++ Essential/Essential_L7/Essential_L7.1/Train.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L7._1
+{
+    struct Train
+    {
+        private string _destination;
+        private uint _number;
+        private DateTime _departureTime;
+
+        public Train(string destination, uint number, string departureTime)
+        {
+            _destination = destination;
+            _number = number;
+            _departureTime = DateTime.Parse(departureTime);
+        }
+
+        public string Destination { get { return _destination; } }
+        public uint Number { get { return _number; } }
+        public DateTime DepartureTime { get { return _departureTime; } }
+
+        public override string ToString()
+        {
+            return string.Format("Train Information:{0}Destination: {1}{0}Number: {2}{0}Departure time: {3:HH:mm}", Environment.NewLine, Destination, Number, DepartureTime);
+        }
+    }
+}
