0fedd2348e7e90b39748705978ebee598de4c8dd
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-28 11:57:35 -0400

    Implement project Essential_L7 Add and implement structure Notebook.cs Update gitignore

---
+++ Essential/Essential_L7/Essential_L7/Notebook.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L7
+{
+    struct Notebook
+    {
+        private string _model, _producer;
+        private double _price;
+
+        public Notebook(string model, string producer, double price)
+        {
+            _model = model;
+            _producer = producer;
+            _price = Math.Abs(price);
+        }
+
+        public string Model { get { return _model; } }
+        public string Producer { get { return _producer; } }
+        public double Price { get { return _price; } }
+
+        public void ShowNotebook()
+        {
+            Console.WriteLine("Model: {1}{0}Producer: {2}{0}Price: {3:N} $", Environment.NewLine, Model, Producer, Price);
+        }
+    }
+}
---
+++ Essential/Essential_L7/Essential_L7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook myBook = new Notebook("ProBook 470 G1", "HP", 794);
+            myBook.ShowNotebook();
+        }
+    }
+}
