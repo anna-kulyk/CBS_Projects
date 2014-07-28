85b297c99f12d79fad5bbe1abfdf2890038ec994
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-28 19:18:37 -0400

    Implement project Essential_L7.2 Update gitignore

---
+++ Essential/Essential_L7/Essential_L7.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L7._2
+{
+    class Program
+    {
+        static void ClassTaker(MyClass myclass)
+        {
+            myclass.change = "changed";
+        }
+
+        static void StructTaker(MyStruct myStruct)
+        {
+            myStruct.change = "changed";
+            //Console.WriteLine(myStruct.change);
+        }
+
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+            ClassTaker(mc);
+            Console.WriteLine(mc.change);
+            mc.change = "not changed";
+            Console.WriteLine(mc.change);
+            Console.WriteLine(new string('-', 40));
+
+            MyStruct ms = new MyStruct();
+            StructTaker(ms);
+            Console.WriteLine(ms.change);
+            ms.change = "not changed";
+            Console.WriteLine(ms.change);
+        }
+    }
+}
