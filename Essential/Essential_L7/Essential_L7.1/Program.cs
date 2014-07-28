991b47b8f94f34ed6d01cc76a5d96afd60ffaea1
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-28 18:11:50 -0400

    Update method AddTrains

--- Essential/Essential_L7/Essential_L7.1/Program.cs
+++ Essential/Essential_L7/Essential_L7.1/Program.cs
@@ -8,23 +8,44 @@ namespace Essential_L7._1
 {
     class Program
     {
-        static void AddTrains(Train[] trains)
+        static void AddTrains(ref Train[] trains)
         {
-            for (int i = 0; i < trains.Length; i++)
+            if (trains == null)
             {
-                Console.Write("Enter the destination: ");
-                string destination = Console.ReadLine();
-                Console.Write("Enter the number of the train: ");
-                uint number = Convert.ToUInt32(Console.ReadLine());
-                Console.Write("Enter the departure time: ");
-                string departureTime = Console.ReadLine();
-                Console.WriteLine(new string('-', 30));
-                trains[i] = new Train(destination, number, departureTime);
+                trains = new Train[0];
+            }
+
+            while (true)
+            {
+                Console.Write("Do you whant to add new train? (yes / no): ");
+                string result = Console.ReadLine();
+                if (result == "yes")
+                {
+                    Array.Resize<Train>(ref trains, (trains.Length + 1));
+                    Console.Write("Enter the destination: ");
+                    string destination = Console.ReadLine();
+                    Console.Write("Enter the number of the train: ");
+                    uint number = Convert.ToUInt32(Console.ReadLine());
+                    Console.Write("Enter the departure time: ");
+                    string departureTime = Console.ReadLine();
+                    Console.WriteLine(new string('-', 30));
+                    trains[trains.Length - 1] = new Train(destination, number, departureTime);
+
+                }
+                else if (result == "no")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You have entered the wrong key!");
+                }
             }
         }
 
+
         static void Sort(Train[] trains)
-        {            
+        {
             for (int i = 0; i < trains.Length; i++)
             {
                 for (int j = 0; j < trains.Length; j++)
@@ -34,13 +55,13 @@ namespace Essential_L7._1
                         Train t = trains[i];
                         trains[i] = trains[j];
                         trains[j] = t;
-                        
+
                     }
                 }
             }
         }
 
-        static void ShowTrainByNumber (Train[] trains, uint number)
+        static void ShowTrainByNumber(Train[] trains, uint number)
         {
             int counter = 0;
             for (int i = 0; i < trains.Length; i++)
@@ -49,25 +70,25 @@ namespace Essential_L7._1
                 {
                     counter++;
                     Console.WriteLine(trains[i].ToString());
-                }                
+                }
             }
             if (counter == 0)
             {
-               Console.WriteLine("The train # {0} doesn't exists!", number); 
+                Console.WriteLine("The train # {0} doesn't exists!", number);
             }
         }
 
 
         static void Main(string[] args)
         {
-            var trains = new Train[3];
-            AddTrains(trains);
+            Train[] trains = new Train[0];
+            AddTrains(ref trains);
             Sort(trains);
             Console.WriteLine("You have entered the following data:");
             foreach (var item in trains)
             {
                 Console.WriteLine(new string('*', 20));
-                Console.WriteLine(item);                
+                Console.WriteLine(item);
             }
             Console.WriteLine(new string('-', 30));
             Console.Write("Enter the number of the train to start the search: ");
