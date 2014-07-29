bb9bc0ac3c79a76bd4d4a68f6bb552dc720e663c
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-29 13:36:31 -0400

    Implement project Essential_L8 Update gitignore

---
+++ Essential/Essential_L8/Essential_L8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your birthday: ");
+            DateTime birthday = DateTime.Parse(Console.ReadLine());
+            DateTime now = DateTime.Now;            
+            DateTime nextBirthday = new DateTime(now.Year, birthday.Month, birthday.Day);
+            TimeSpan delta;
+            if (nextBirthday.DayOfYear > now.DayOfYear)
+            {
+                delta = nextBirthday - now;
+            }                      
+            else
+            {
+                DateTime _nextBirthday = new DateTime(now.Year + 1, birthday.Month, birthday.Day);
+                delta = _nextBirthday - now;
+            }   
+            Console.WriteLine((delta.Days + 1) + " day(s) left till your next birthday!");
+        }
+    }
+}
