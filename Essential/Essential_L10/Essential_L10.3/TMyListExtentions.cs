76df07451fb5f2ae848f7a9051c68789c121c23e
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-31 16:29:38 -0400

    Implement project Essential_L10.3 (Generics) Add and implement generic class MyList.cs and static class TMyListExtentions.cs Update gitignore

---
+++ Essential/Essential_L10/Essential_L10.3/TMyListExtentions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L10._3
+{
+    static class TMyListExtentions
+    {
+        public static T[] GetArray<T>(this MyList<T> list)
+        {
+            T[] array = new T[list.ListLength];
+            for (uint i = 0; i < list.ListLength; i++)
+            {
+                array[i] = list[i];
+            }
+            return array;
+        }
+    }
+}
