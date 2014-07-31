8157537a6fb18cc427805f363b5fe1761b50bb6d
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-31 13:04:55 -0400

    Implement project Essential_L10 (Generics) Add and implement generic class MyClass.cs Update gitignore

---
+++ Essential/Essential_L10/Essential_L10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = MyClass<int>.FactoryMethod();
+            Console.WriteLine(i);
+        }
+    }
+}
