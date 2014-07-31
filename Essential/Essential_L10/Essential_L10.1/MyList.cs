e5f3781359d6e65062d6b9becffa60ef2e4e9902
Anna Kulyk <kulyk_anna@outlook.com>
2014-07-31 13:48:33 -0400

    Implement project Essential_L10.1 (Generics) Add and implement generic class MyList.cs Update gitignore

---
+++ Essential/Essential_L10/Essential_L10.1/MyList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L10._1
+{
+    class MyList<T>
+    {
+        T[] list = new T[0];
+
+        public int ListLength
+        {
+            get
+            {
+                return list.Length;
+            }
+        }
+
+        public void AddItem(T item)
+        {
+            Array.Resize<T>(ref list, (list.Length + 1));
+            list[list.Length - 1] = item;
+        }
+
+        public T this[uint index]
+        {
+            get
+            {
+                if (index < list.Length)
+                {
+                    return list[index];
+                }
+                else
+                {
+                    Console.WriteLine("Index is out of range!");
+                    return default(T);
+                }                
+            }
+        }
+    }
+}
---
+++ Essential/Essential_L10/Essential_L10.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list = new MyList<int>();            
+            list.AddItem(8);
+            list.AddItem(6);
+            list.AddItem(4);
+            list.AddItem(2);
+            list.AddItem(0);
+            Console.WriteLine(list.ListLength);
+            Console.WriteLine(new string('-', 10));
+            for (uint i = 0; i < list.ListLength; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            Console.WriteLine(new string('-', 10));
+            Console.WriteLine(list[4]);
+            Console.WriteLine(list[5]);
+        }
+    }
+}
