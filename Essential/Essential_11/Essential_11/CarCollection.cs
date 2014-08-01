5b3efb1668efb246ae655c876f86e8d8579648b5
Anna Kulyk <kulyk_anna@outlook.com>
2014-08-01 12:33:07 -0400

    Implement project Essential_L11.1 (Generics) Add and implement generic class CarCollection.cs and class Car.cs Update gitignore

---
+++ Essential/Essential_11/Essential_11/CarCollection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_11
+{
+    class CarCollection<T> where T : Car, new ()
+    {
+        T[] cars = new T[0];
+
+        public int CarCount
+        {
+            get
+            {
+                return cars.Length;
+            }
+        }
+
+        public void AddCar(string name, uint year)
+        {
+            Array.Resize<T>(ref cars, (cars.Length + 1));
+            cars[cars.Length - 1] = new T();
+            cars[cars.Length - 1].Name = name;
+            cars[cars.Length - 1].Year = year;
+
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >=0 && index < cars.Length)
+                {
+                    return cars[index].ToString();
+                }
+                else
+                {
+                   return string.Format("Index is out of range!");
+                }
+            }
+        }
+    }
+}
