42663e65bc33c890adccde202acc0fabee9f731c
Anna Kulyk <kulyk_anna@outlook.com>
2014-08-04 12:32:11 -0400

    Implement project Essential_L12 (Events) Add the "add" and "remove" event accessor blocks into "003_MPV" project Update gitignore

---
+++ Essential/Essential_L12/Essential_L12/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+
+// View
+
+namespace MVP
+{    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            new Presenter(this);
+        }
+
+        EventHandler myEvent = null;
+        public event EventHandler MyEvent
+        {
+            add
+            {
+                myEvent += value;             
+            }
+            remove
+            {
+                myEvent -= value;
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            myEvent.Invoke(sender, e);
+        }
+    }
+}
