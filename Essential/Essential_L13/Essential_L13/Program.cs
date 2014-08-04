81f709aa4856405b8d91d68b248e607cd0258985
Anna Kulyk <kulyk_anna@outlook.com>
2014-08-04 16:46:17 -0400

    Implement project Essential_L13 (Threads) Update gitignore

---
+++ Essential/Essential_L13/Essential_L13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Essential_L13
+{
+    class Program
+    {
+        static int count = 5;
+
+        static void MyMethod()
+        {
+            Console.WriteLine("Thread: {0}, count = {1}", Thread.CurrentThread.GetHashCode(), count);
+            if (count > 0)
+            {
+                --count;
+                Thread th = new Thread(MyMethod);
+                th.Start();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod();
+        }
+    }
+}
