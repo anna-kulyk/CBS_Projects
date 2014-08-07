5fdc5d7e0c965ba204c5fdcfbd818d92fb0fd5d2
Anna Kulyk <kulyk_anna@outlook.com>
2014-08-07 09:20:39 -0400

    Sketch of the project Essential_L13.1 (Threads - Matrix) Update gitignore

---
+++ Essential/Essential_L13/Essential_L13.1/Matrix.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Essential_L13._1
+{
+    class Matrix
+    {
+        public void MatrixString(object x)
+        {
+            object locker = new object();
+
+            string[] array = new string[9];
+            array[0] = "f";
+            array[1] = "y";
+            array[2] = "r";
+            array[3] = "v";
+            array[4] = "d";
+            array[5] = "g";
+            array[6] = "a";
+            array[7] = "s";
+            array[8] = "t";
+
+
+            while (true)
+            {
+                for (int j = 0; j < Console.WindowHeight + array.Length; j++)
+                {
+                    int index = j;
+                    if (j < array.Length)
+                    {
+                        for (int i = 0; i <= j; i++)
+                        {
+
+                            Console.SetCursorPosition((int)x, index);
+                            if (i == 0)
+                            {
+                                Console.ForegroundColor = ConsoleColor.White;
+                            }
+                            else if (i == 1)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            }
+                            Console.WriteLine(array[i]);
+                            index--;
+                        }
+                        Thread.Sleep(100);
+                    }
+                    else
+                    {
+                        for (int i = 0; i <= array.Length; i++)
+                        {
+                            if (j < Console.WindowHeight && i != array.Length)
+                            {
+
+                                Console.SetCursorPosition((int)x, index);
+                                if (i == 0)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                }
+                                else if (i == 1)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                                }
+                                Console.WriteLine(array[i]);
+                                index--;
+
+                            }
+                            else if (i != array.Length)
+                            {
+                                if (i >= j - Console.WindowHeight + 1)
+                                {
+                                    index = j - i;
+                                    Console.SetCursorPosition((int)x, index);
+                                    Console.WriteLine(array[i]);
+                                    index--;
+                                }
+                            }
+                            else
+                            {
+                                index = j - i;
+                                Console.SetCursorPosition((int)x, index);
+                                Console.WriteLine(" ");
+                            }
+                        }
+                        Thread.Sleep(100);
+                        Console.SetCursorPosition((int)x, index);
+                        Console.WriteLine(" ");
+                    }
+                }
+            }
+        }
+    }
+}
--- Essential/Essential_L13/Essential_L13.1/Program.cs
+++ Essential/Essential_L13/Essential_L13.1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Essential_L13._1
@@ -10,6 +11,24 @@ namespace Essential_L13._1
     {
         static void Main(string[] args)
         {
+            Console.SetWindowSize(90, 35);
+            Random rand = new Random();
+
+            //Matrix m2 = new Matrix();
+            //m2.MatrixString(3);
+
+            Matrix m = new Matrix();
+            ParameterizedThreadStart matrix = new ParameterizedThreadStart(m.MatrixString);
+            
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Thread matrixThread = new Thread(matrix);
+                matrixThread.Start(rand.Next(1, Console.WindowWidth - 1));
+                Thread.Sleep(rand.Next(200, 2000));
+            }
+
+
+
         }
     }
 }
