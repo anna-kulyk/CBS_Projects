a3368f09861723fb737e6bd12a90d223ec65d51f
Anna Kulyk <kulyk_anna@outlook.com>
2014-08-07 12:39:27 -0400

    Implement project Essential_L14 (Collections) Add and implement class MyIntCollection Update gitignore
    Conflicts:
    	.gitignore

---
+++ Essential/Essential_L14/Essential_L14/MyCollection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L14
+{
+    class MyIntCollection
+    {
+        private int[] myArray;
+        
+        public MyIntCollection(int length)
+        {
+            myArray = new int[length];
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = i;
+            }            
+        }
+        
+        public IEnumerable EvenNumbers()
+        {
+            foreach (var item in myArray)
+            {
+                if (item % 2 == 0)
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
---
+++ Essential/Essential_L14/Essential_L14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var intCollection = new MyIntCollection(10);
+            foreach (var item in intCollection.EvenNumbers())
+            {
+                Console.Write("{0} ", item);
+            }
+        }
+    }
+}
