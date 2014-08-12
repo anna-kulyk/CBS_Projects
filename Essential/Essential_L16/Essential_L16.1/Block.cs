e6a3fc0b0b28635c0ae7a41c33d61da66e2af1fa
Anna Kulyk <kulyk_anna@outlook.com>
2014-08-12 15:14:32 -0400

    Implement project Essential_L16.1 (Object class) Add and implement class Block.cs. Override methods Equals and ToString. Update gitignor

---
+++ Essential/Essential_L16/Essential_L16.1/Block.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L16._1
+{
+    class Block
+    {
+        public Block(uint ab, uint bc, uint cd, uint da)
+        {
+            AB = ab;
+            BC = bc;
+            CD = cd;
+            DA = da;
+        }
+
+        public uint AB { get; set; }
+        public uint BC { get; set; }
+        public uint CD { get; set; }
+        public uint DA { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Block)
+            {
+                Block block = (Block)obj;
+                if (this.AB == block.AB && this.BC == block.BC && this.CD == block.CD && this.DA == block.DA)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0}, {1}, {2}, {3})", AB, BC, CD, DA);
+        }
+    }
+}
---
+++ Essential/Essential_L16/Essential_L16.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L16._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b1 = new Block(1, 2, 3, 4);
+            var b2 = new Block(2, 3, 4, 5);
+            var b3 = b1;
+
+            Console.WriteLine(b1.Equals(b2));
+            Console.WriteLine(b1.Equals(b3));
+            Console.WriteLine(b1.ToString());
+        }
+    }
+}
