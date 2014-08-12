be901b5150c0f5c580bda9cacc15571401c34c67
Anna Kulyk <kulyk_anna@outlook.com>
2014-08-12 12:57:06 -0400

    Implement project Essential_L16 (Overloadable Operators) Add and implement structure Point.cs. Overload the operator +. Update gitignore

---
+++ Essential/Essential_L16/Essential_L16/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Point(1, 1, 1);
+            var b = new Point(2, 2, 2);
+            var c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
