fe1f7cda02d8605287a3b5383011e5117b65298a
Anna Kulyk <kulyk_anna@outlook.com>
2014-08-13 13:13:46 -0400

    Implement project Essential_L17 (dynamic) Add and implement class Calculator.cs.cs. Update gitignor

---
+++ Essential/Essential_L17/Essential_L17/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L17
+{
+    static class Calculator
+    {
+        public static dynamic Add(dynamic operand1, dynamic operand2)
+        {
+            return operand1 + operand2;
+        }
+
+        public static dynamic Subtract(dynamic operand1, dynamic operand2)
+        {
+            return operand1 - operand2;
+        }
+
+        public static dynamic Multiply(dynamic operand1, dynamic operand2)
+        {
+            return operand1 * operand2;
+        }
+
+        public static dynamic Divide(dynamic operand1, dynamic operand2)
+        {
+            if (operand2 == 0)
+            {
+                return "You cannot divide by zero!";
+            }
+
+            return operand1 / operand2;
+        }
+    }
+}
---
+++ Essential/Essential_L17/Essential_L17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var operand1 = 20;
+            var operand2 = 10;
+
+            Console.WriteLine("{0} + {1} = {2}", operand1, operand2, Calculator.Add(operand1, operand2));
+            Console.WriteLine("{0} - {1} = {2}", operand1, operand2, Calculator.Subtract(operand1, operand2));
+            Console.WriteLine("{0} * {1} = {2}", operand1, operand2, Calculator.Multiply(operand1, operand2));
+            Console.WriteLine("{0} / {1} = {2}", operand1, operand2, Calculator.Divide(operand1, operand2));
+        }
+    }
+}
