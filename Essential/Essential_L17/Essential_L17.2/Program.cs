060ceee72af41da5f216d788146c096f0928c820
Anna Kulyk <kulyk_anna@outlook.com>
2014-08-13 15:08:38 -0400

    Implement project Essential_L17.2 (Anonymous Types) Update gitignor

---
+++ Essential/Essential_L17/Essential_L17.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential_L17._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new[]
+            {
+                new {English = "apple", Russian = "яблоко"},
+                new {English = "pear", Russian = "груша"},
+                new {English = "orange", Russian = "апельсин"},
+                new {English = "lemon", Russian = "лимон"},
+                new {English = "pineapple", Russian = "ананас"},
+                new {English = "blueberry", Russian = "черника"},
+                new {English = "strawberry", Russian = "клубника"},
+                new {English = "raspberry", Russian = "малина"},
+                new {English = "cherry", Russian = "вишня"},
+                new {English = "banana", Russian = "банан"},
+            };
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine("{0} - {1}", item.English, item.Russian);
+            }
+        }
+    }
+}
