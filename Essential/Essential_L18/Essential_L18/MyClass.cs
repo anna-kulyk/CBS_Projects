5af9cc8a82f0efce528a4d2d762da6337c27339d
Anna Kulyk <kulyk_anna@outlook.com>
2014-08-14 13:22:20 -0400

    Implement project Essential_L18 (Namespace) Add and implement class MyClass.cs in namespace MyNamespace Update gitignor

---
+++ Essential/Essential_L18/Essential_L18/MyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace MyNamespace
+{
+    class MyClass
+    {
+        public MyClass()
+        {
+            Console.WriteLine("MyNamespace.MyClass");
+        }
+    }
+}
---
+++ Essential/Essential_L18/Essential_L18/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+using MyNamespace;
+
+namespace Essential_L18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+        }
+    }
+}
