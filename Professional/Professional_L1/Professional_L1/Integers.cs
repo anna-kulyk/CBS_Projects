7cae2b16c62562b93dfb0345458631c6b82e05c5
Anna Kulyk <kulyk_anna@outlook.com>
2014-08-18 13:14:43 -0400

    Implement project Professional_L1 (Collections) Add and implement class Integers.cs and static method GetSquared() Update gitignor

---
+++ Professional/Professional_L1/Professional_L1/Integers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Professional_L1
+{
+    class Integers: IEnumerable
+    {
+        private int[] _array = new int[0];
+
+        public Integers(int[] array)
+        {
+            _array = array;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                yield return _array[i];
+            }
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return _array[index];
+            }
+        }
+    }
+}
---
+++ Professional/Professional_L1/Professional_L1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Professional_L1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Integers integersSquared = GetSquared(integers);
+
+            foreach (var item in integersSquared)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static Integers GetSquared(int[] array)
+        {
+            int[] newArray = new int[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+			{
+                newArray[i] = array[i] * array[i];			 
+			}
+
+            return new Integers(newArray);
+        }
+    }
+}
