09a54a70dd9b462c09aa7ecbd05a4d58a816a530
Anna Kulyk <kulyk_anna@outlook.com>
2014-08-19 12:21:37 -0400

    Implement project Professional_L1.2 (Collections) Add and implement classes Person.cs and CollectionGenericPersonExtensions.cs Update gitignor

---
+++ Professional/Professional_L1/Professional_L1.2/CollectionGenericPersonExtensions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Professional_L1._2
+{
+    static class CollectionGenericPersonExtensions
+    {
+        public static Person FindByName(this Collection<Person> persons, string name)
+        {
+            Person result = null;
+
+            foreach (var person in persons)
+            {
+                if (person.Name == name)
+                {
+                    result = person;
+                }
+                else if (person.Heirs != null && person.Heirs.Any())
+                {
+                    result = FindByName(person.Heirs, name);
+                }
+            }
+
+            return result;
+        }
+
+        public static void AddHeir(this Collection<Person> persons, string parentName, Person heir)
+        {
+            Person parent = persons.FindByName(parentName);
+            if(parent != null)
+            {
+                parent.Heirs.Add(heir);
+            }
+            else
+            {
+                Console.WriteLine("There is no {0} in this family", parentName);
+            }
+        }
+
+        public static void ShowHeirs(this Collection<Person> persons, string parentName)
+        {
+            Person parent = persons.FindByName(parentName);
+            Console.WriteLine("{0} heirs:", parentName);
+            foreach (var item in parent.Heirs)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+
+        public static void SelectByYear(this Collection<Person> sortedByYear, Collection<Person> persons, uint year)
+        {
+            foreach (var person in persons)
+            {
+                if (person.Year == year)
+                {
+                    sortedByYear.Add(person);
+                }
+                else if(person.Heirs != null && person.Heirs.Any())
+                {
+                    SelectByYear(sortedByYear, person.Heirs, year);
+                }
+            }
+        }
+    }
+}
---
+++ Professional/Professional_L1/Professional_L1.2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Professional_L1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var siriusBlack = new Person("Sirius Black", 1959);
+            var regulusBlack = new Person("Regulus Black", 1961);
+            var orionBlack = new Person("Orion Black", 1929, new Collection<Person> { siriusBlack, regulusBlack });
+            var alphardBlack = new Person("Alphard Black", 1930);
+            var nymphadoraTonks = new Person("Nymphadora Tonks", 1973);
+            var andromedaBlack = new Person("Andromeda Black", 1953, new Collection<Person> { nymphadoraTonks });
+            var bellatrixBlack = new Person("Bellatrix Black", 1951);
+            var dracoMalfoy = new Person("Draco Malfoy", 1980);
+            var narcissaBlack = new Person("Narcissa Black", 1955, new Collection<Person> { dracoMalfoy });
+            var cygnusBlack = new Person("Cygnus Black", 1938, new Collection<Person> { bellatrixBlack, andromedaBlack, narcissaBlack });
+            var lucretiaBlack = new Person("Lucretia Black", 1925);
+
+            var familyTree = new Collection<Person> 
+            {
+                orionBlack, alphardBlack, cygnusBlack, lucretiaBlack              
+            };
+            
+            var draco = familyTree.FindByName("Draco Malfoy");
+            Console.WriteLine(draco.Name);
+
+            var scorpiusMalfoy = new Person("Scorpius Malfoy", 2006);
+            familyTree.AddHeir("Draco Malfoy", scorpiusMalfoy);
+
+            familyTree.ShowHeirs("Draco Malfoy");
+
+            Collection<Person> sorted = new Collection<Person>();
+            sorted.SelectByYear(familyTree, 1959);
+            foreach (var item in sorted)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+    }
+}
