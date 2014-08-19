3a92bc4b0ddfec8c9d0b0313d822f38cb3373289
Anna Kulyk <kulyk_anna@outlook.com>
2014-08-19 13:49:31 -0400

    Implement project Professional_L1.3 (Collections) Add and implement classes Word.cs, CollectionGenericWordExtensions.cs and enumeration Language.cs Update gitignor

---
+++ Professional/Professional_L1/Professional_L1.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Professional_L1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Collection<Word>();
+            dictionary.Add(new Word("book", "книга", "книжка"));
+            dictionary.Add(new Word("pen", "ручка", "ручка"));
+            dictionary.Add(new Word("sun", "солнце", "сонце"));
+            dictionary.Add(new Word("apple", "яблоко", "яблуко"));
+            dictionary.Add(new Word("table", "стол", "стіл"));
+
+            dictionary.TranslationFromUkrainian("яблуко", Language.EN);            
+        }
+    }
+}
