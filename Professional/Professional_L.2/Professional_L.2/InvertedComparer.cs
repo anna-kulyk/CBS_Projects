373c8adbce2a5c51f35d77652bcf2bcc31942085
Anna Kulyk <kulyk_anna@outlook.com>
2014-08-20 11:31:14 -0400

    Implement project Professional_L2 (Collections) Add and implement class InvertedComparer.cs Update gitignor

---
+++ Professional/Professional_L.2/Professional_L.2/InvertedComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Professional_L._2
+{
+    class InvertedComparer : IComparer
+    {
+        public int Compare(Object x, Object y)
+        {
+            return ((new CaseInsensitiveComparer()).Compare(y, x));
+        }
+    }
+}
