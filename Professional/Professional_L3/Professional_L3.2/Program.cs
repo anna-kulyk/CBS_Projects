2649da4df7bc17fcc96bf8c8e975272117811699
Anna Kulyk <kulyk_anna@outlook.com>
2014-08-22 13:01:07 -0400

    Implement project Professional_L3.2 (IO) Add and implement class Searcher.cs Update gitignor

---
+++ Professional/Professional_L3/Professional_L3.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Professional_L3._2
+{
+    //Напишите приложение для поиска заданного файла на диске. Добавьте код, 
+    //использующий класс FileStream и позволяющий просматривать файл в текстовом окне. 
+    //В заключение добавьте возможность сжатия найденного файла.
+
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file name: ");
+            string fileName = Console.ReadLine();
+            string root = @"H:\";
+            FileInfo fileInfo = Searcher.SearchFile(fileName, root);
+            if (fileInfo != null)
+            {
+                Console.WriteLine("Found: {0}", fileInfo.FullName);
+                Console.WriteLine();
+
+                using (var fs = new FileStream(fileInfo.FullName, FileMode.Open, FileAccess.Read))
+                {
+                    using (var sr = new StreamReader(fs))
+                    {
+                        string line = sr.ReadLine();
+
+                        while (line != null)
+                        {
+                            Console.WriteLine(line);
+                            line = sr.ReadLine();
+                        }
+                    }
+                }                
+            }
+            Console.ReadKey();            
+        }
+    }
+}
+
---
+++ Professional/Professional_L3/Professional_L3.2/Searcher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Professional_L3._2
+{
+    static class Searcher
+    {
+        static public FileInfo SearchFile(string fileName, string root)
+        {
+            var dir = new DirectoryInfo(root);
+            FileInfo[] files = dir.GetFiles(string.Format("*{0}*", fileName));
+            if (files.Length > 0)
+            {
+                return files[0];
+            }
+            else
+            {
+                DirectoryInfo[] dirs = dir.GetDirectories();
+                if (dirs.Length > 0)
+                {
+                    foreach (var item in dirs)
+                    {
+                        if (item.Attributes.Equals(FileAttributes.System | FileAttributes.Hidden | FileAttributes.Directory))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            FileInfo file = SearchFile(fileName, item.FullName);
+                            return file;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Cannot find file {0}", fileName);
+                }
+                return null;
+            }
+        }
+    }
+}
