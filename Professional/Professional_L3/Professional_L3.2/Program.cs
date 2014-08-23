35b1fcffa5b36208c82152366c27e4a36f3eac63
Anna Kulyk <kulyk_anna@outlook.com>
2014-08-23 12:16:48 -0400

    Implement project Professional_L3.3 (IsolatedStorage) Improve project Professional_L3.2 Update gitignor

--- Professional/Professional_L3/Professional_L3.2/Program.cs
+++ Professional/Professional_L3/Professional_L3.2/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.IO.IsolatedStorage;
+using System.IO.Compression;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,34 +17,53 @@ namespace Professional_L3._2
     {   
         static void Main(string[] args)
         {
-            Console.Write("Enter file name: ");
-            string fileName = Console.ReadLine();
-            string root = @"H:\";
-            FileInfo fileInfo = Searcher.SearchFile(fileName, root);
-            if (fileInfo != null)
-            {
-                Console.WriteLine("Found: {0}", fileInfo.FullName);
-                Console.WriteLine();
 
-                using (var fs = new FileStream(fileInfo.FullName, FileMode.Open, FileAccess.Read))
-                {
-                    using (var sr = new StreamReader(fs))
-                    {
-                        string line = sr.ReadLine();
+            IsolatedStorageFile userStorage = IsolatedStorageFile.GetUserStoreForAssembly();
+            Console.WriteLine(userStorage.ToString());
 
-                        while (line != null)
-                        {
-                            Console.WriteLine(line);
-                            line = sr.ReadLine();
-                        }
-                    }
-                }                
-            }
-            else
-            {
-                Console.WriteLine("Cannot find file {0}", fileName);
-            }
-            Console.ReadKey();            
+        //    Console.Write("Enter file name: ");
+        //    string fileName = Console.ReadLine();
+        //    string root = @"H:\";
+        //    FileInfo fileInfo = Searcher.SearchFile(fileName, root);
+        //    if (fileInfo != null)
+        //    {
+        //        Console.WriteLine("Found: {0}", fileInfo.FullName);
+        //        Console.WriteLine();
+
+        //        using (var fs = new FileStream(fileInfo.FullName, FileMode.Open, FileAccess.Read))
+        //        {
+        //            using (var sr = new StreamReader(fs))
+        //            {
+        //                string line = sr.ReadLine();
+
+        //                while (line != null)
+        //                {
+        //                    Console.WriteLine(line);
+        //                    line = sr.ReadLine();
+        //                }
+        //            }
+        //        }
+
+        //        using (var fs = new FileStream(fileInfo.FullName, FileMode.Open, FileAccess.Read))
+        //        {
+        //            FileStream destination = File.Create(string.Format(fileInfo.Directory + @"\archive.zip"));
+
+        //            using (GZipStream compressor = new GZipStream(destination, CompressionMode.Compress))
+        //            {
+        //                int theByte = fs.ReadByte();
+        //                while (theByte != -1)
+        //                {
+        //                    compressor.WriteByte((byte)theByte);
+        //                    theByte = fs.ReadByte();
+        //                }
+        //            } 
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Cannot find file {0}", fileName);
+        //    }  
+        //    Console.ReadKey();            
         }
     }
 }
