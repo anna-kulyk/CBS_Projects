55fc6e6431a6484013ccbcd709bdbde48f968e2f
Anna Kulyk <kulyk_anna@outlook.com>
2014-09-17 16:29:03 -0400

    Implement projects Professional_L4 and Professional_L4.1(Regular Expression) Update gitignor

---
+++ Professional/Professional_L4/Professional_L4.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Professional_L4._1
+{
+    /*
+     * Задание 3 
+     * Напишите шуточную программу «Дешифратор», которая бы в текстовом файле могла бы заменить все предлоги на слово «ГАВ!».
+     * (На данный момент замена происходит не в текстовом файле, а в предложении, ввдененном пользователем.)
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[а-я]{1,3}\b";
+            string sentence;
+
+            Console.WriteLine("Введите предложение:");
+            sentence = Console.ReadLine();
+
+            string result = Regex.Replace(sentence, pattern, "ГАВ!");
+            Console.WriteLine(result);
+        }
+    }
+}
