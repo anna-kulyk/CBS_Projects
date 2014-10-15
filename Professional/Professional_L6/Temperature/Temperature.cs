dd6a757090b66393c8f291cd1904f455f7b0e64d
Anna Kulyk <kulyk_anna@outlook.com>
2014-10-14 20:10:19 -0400

    Implement project Professional_L6.2 and Temperature (Reflection) Update gitignor

---
+++ Professional/Professional_L6/Temperature/Temperature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperature
+{
+    static class Temperature
+    {
+        public static decimal ConvertToFahrenheit(decimal temperature)
+        {
+            return temperature * 9 / 5 + 32;
+        }
+    }
+}
