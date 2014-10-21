0f45bc0623f26ad77ea0e6d05412557d561b9193
Anna Kulyk <kulyk_anna@outlook.com>
2014-10-21 15:13:56 -0400

    Implement project Professional_L7.1 (Attribute) Update gitignor

---
+++ Professional/Professional_L7/Professional_L7.1/MyClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Professional_L7._1
+{
+    class MyClass
+    {
+        [Obsolete("This is an obsolete method")]
+        public void FirstMethod()
+        {
+            Console.WriteLine("This is the first obsolete method");
+        }
+
+        [Obsolete("You cannot use this metod", true)]
+        public void SecondMethod()
+        {
+            Console.WriteLine("This is the second obsolete method");
+        }
+    }
+}
---
+++ Professional/Professional_L7/Professional_L7.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Professional_L7._1
+{
+    /*
+     * Создайте класс и примените к его методам атрибут Obsolete
+     * сначала в форме просто выводящей предупреждение, а затем в форме, препятствующей компиляции. 
+     * Продемонстрируйте работу атрибута на примере вызова данных методов.
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var my = new MyClass();
+
+            my.FirstMethod(); // Выводится предупреждение, что метод устаревший, но компиляция продолжается.
+            my.SecondMethod(); // Выдается ошибка на уровне компиляции.
+
+        }
+    }
+}
