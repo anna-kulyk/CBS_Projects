f74388f3bdacd7839794fb4549fc1647be8fb827
Anna Kulyk <kulyk_anna@outlook.com>
2014-10-23 19:01:09 -0400

    Implement project Professional_L8.1(Serialization XML) Update gitignor

---
+++ Professional/Professional_L8/Professional_L8.1/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using System.Threading.Tasks;
+
+namespace Professional_L8._1
+{
+    public class Address
+    {
+        public string street;
+        public string appartment;
+        public string city;
+        public string country;
+        public uint zip;
+
+        public Address() { }
+    }
+}
---
+++ Professional/Professional_L8/Professional_L8.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using System.Threading.Tasks;
+
+namespace Professional_L8._1
+{
+    /*
+     * Создайте класс, поддерживающий сериализацию. 
+     * Выполните сериализацию объекта этого класса в формате XML. 
+     * Сначала используйте формат по умолчанию, а затем измените его таким образом, 
+     * чтобы значения полей сохранились в виде атрибутов элементов XML .
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var address = new Address { street = "11947 Sentinel Point Ct", city = "Reston, VA", country = "USA", zip = 20191 };
+            var naddress = new NewAddress { street = "11703 Olde English Dr", appartment = "Unit I", city = "Reston, VA", country = "USA", zip = 20190 };
+
+            var stream = new FileStream("First_Address.xml", FileMode.OpenOrCreate);
+            var serializer = new XmlSerializer(typeof(Address));
+            serializer.Serialize(stream, address);
+            stream.Close();
+
+            stream = new FileStream("Second_Address.xml", FileMode.OpenOrCreate);
+            serializer = new XmlSerializer(typeof(NewAddress));
+            serializer.Serialize(stream, naddress);
+            stream.Close();
+        }
+    }
+}
