65acb916680c9ef592efbaede0fa764aa13995fc
Anna Kulyk <kulyk_anna@outlook.com>
2014-10-29 17:57:09 -0400

    Implement project Professional_L9.1(Garbage Collection) Update gitignor

---
+++ Professional/Professional_L9/Professional_L9.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Professional_L9._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    var myClass = new MyClass();
+                    Console.WriteLine("Object {0} is created", myClass.GetHashCode());
+                    myClass.Dispose();
+                }
+            }
+            catch (OutOfMemoryException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
