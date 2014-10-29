84cec746436734b4c12f12fb7ab624fcb9113afe
Anna Kulyk <kulyk_anna@outlook.com>
2014-10-29 17:17:38 -0400

    Implement project Professional_L9(Garbage Collection) Update gitignor

---
+++ Professional/Professional_L9/Professional_L9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Professional_L9
+{
+    /*
+     * Создайте свой класс, объекты которого будут занимать много места в памяти
+     * (например, в коде класса будет присутствовать большой массив)
+     * и реализуйте для этого класса формализованный шаблон очистки.
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    var myClass = new MyClass();
+                    Console.WriteLine("Object {0} is created", myClass.GetHashCode());
+                    myClass.Dispose();
+                }
+            }
+            catch (OutOfMemoryException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }            
+        }
+    }
+}
