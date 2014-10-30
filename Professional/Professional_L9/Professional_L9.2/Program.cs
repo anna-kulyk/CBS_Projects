2256b88d232d064526d811c758c8e00ad94cf117
Anna Kulyk <kulyk_anna@outlook.com>
2014-10-30 16:26:23 -0400

    Implement project Professional_L9.2(Garbage Collection) Update gitignor

---
+++ Professional/Professional_L9/Professional_L9.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Professional_L9._2
+{
+    /*
+     * Создайте класс, который позволит выполнять мониторинг ресурсов, используемых программой. 
+     * Используйте его в целях наблюдения за работой программы, а именно: 
+     * пользователь может указать приемлемые уровни потребления ресурсов (памяти), 
+     * а методы класса позволят выдать предупреждение, когда количество реально используемых ресурсов 
+     * приблизиться к максимально допустимому уровню.
+    */
+
+    class LargeObject
+    {
+        int[] array = new int[1000000];
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new LargeObject[1000];
+            var limit = 1000000000;
+            long totalMemory;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new LargeObject();
+                totalMemory = GC.GetTotalMemory(false);
+
+                if (totalMemory > limit)
+                {
+                    Console.WriteLine("You reached the allocated memory limit!");
+                    break;
+                }
+
+                Console.WriteLine("Counter: {0}", i);
+                Console.WriteLine("Total allocated memory: {0}", totalMemory);
+            }
+        }
+    }
+}
