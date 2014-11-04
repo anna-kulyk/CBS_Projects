f97ae8ccee9b4a34165d66240a539c8006b8ec77
Anna Kulyk <kulyk_anna@outlook.com>
2014-11-04 17:02:35 -0500

    Implement class BaseClass and project Professional_L10 (NVI - Versioning) Update gitignor

---
+++ Professional/Professional_L10/BaseClass/BaseClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base
+{
+    public class BaseClass
+    {
+        public void DoWork()
+        {
+            Console.WriteLine("BaseClass.DoWork()");
+            DoSpecificWork();
+        }
+
+        protected virtual void DoSpecificWork()
+        {
+            Console.WriteLine("BaseClass.DoSpecificWork()");
+        }
+    }
+}
