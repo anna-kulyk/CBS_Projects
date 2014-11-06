0a13c321c2fa435ac329214f7dffbc72eb6b4345
Anna Kulyk <kulyk_anna@outlook.com>
2014-11-06 17:53:34 -0500

    Implement project Professional_L10.1 (Template method pattern) Update gitignor

---
+++ Professional/Professional_L10/Professional_L10.1/AbstractClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Professional_L10._1
+{
+    public abstract class AbstractClass
+    {
+        public abstract void PrimitiveOperation1();
+        public abstract void PrimitiveOperation2();
+
+        public void TemplateMethod()
+        {
+            PrimitiveOperation1();
+            PrimitiveOperation2();
+            Console.WriteLine(new string('*', 35));
+        }
+    }
+}
---
+++ Professional/Professional_L10/Professional_L10.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Professional_L10._1
+{
+    /* Выучите описание шаблона Template method (Шаблонный метод). 
+     * Обратите внимание на применимость шаблона, а также на состав его участников и связи отношения между ними. 
+     * Напишите небольшую программу на языке C#, представляющую собой абстрактную реализацию данного шаблона.
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClass[] array = { new ConcreteClassA(), new ConcreteClassB() };
+
+            foreach (var item in array)
+            {
+                item.TemplateMethod();
+            }
+        }
+    }
+}
