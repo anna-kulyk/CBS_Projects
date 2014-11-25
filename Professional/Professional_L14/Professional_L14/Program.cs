3317aff3b08d5e450e6fffa75bf11879bee8550b
Anna Kulyk <kulyk_anna@outlook.com>
2014-11-25 17:09:44 -0500

    Implement project Professional_L14(PLINQ) Update gitignor

---
+++ Professional/Professional_L14/Professional_L14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Professional_L14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            var numbers = new int[1000000];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = r.Next(1, 1000);
+            }
+            
+            var odds = from number in numbers.AsParallel()
+                       where (number % 2) != 0
+                       select number;
+
+            foreach (var odd in odds)
+            {
+                Console.Write(odd + "  ");
+            }
+        }
+    }
+}
